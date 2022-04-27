3365b727f04369c1a6b295a09deb6d7e3b39146a
Konrad Dybcio <konrad.dybcio@somainline.org>
2022-04-27 22:17:09 +0200

    Initial commit

---
+++ constants.py
@@ -0,0 +1,155 @@
+architecture_dict = {
+        0b0000: "Unknown (0b0000)",
+        0b0001: "Armv4",
+        0b0010: "Armv4T",
+        0b0011: "Armv5 (obsolete)", # you don't say, xD
+        0b0100: "Armv5T",
+        0b0101: "Armv5TE",
+        0b0111: "Armv6",
+        0b1111: "Unknown (0b1111)"
+}
+
+implementer_dict = {
+        0x00: "QEMU",
+        0x3e: "Nvidia Corp.",
+        0x41: "ARM Ltd.",
+        0x42: "Broadcom Corp.",
+        0x43: "Cavium Inc.",
+        0x44: "Digital Equipment Corp.",
+        0x46: "Fujitsu Ltd.",
+        0x48: "HiSilicon Technologies Inc.",
+        0x51: "Qualcomm Inc.",
+        0x53: "Samsung",
+        0x54: "Texas Instruments",
+        0x61: "Apple Inc."
+}
+
+partnum_doubledict = {
+        "Apple Inc.": {
+                0x01: "A7 Cyclone"
+        }
+}
+
+# Matrices for checking whether a feature is supported
+# [feature_name, start_bit, stop_bit]
+isar0_matrix = [
+        # 0-3 reserved
+        ["AES", 4, 7],
+        ["SHA1", 8, 11],
+        ["SHA2", 12, 15],
+        ["CRC32", 16, 19],
+        ["ATOMICS", 20, 23],
+        # 24-27 reserved
+        ["RDM", 28, 31],
+        ["SHA3", 32, 35],
+        ["SM3", 36, 39],
+        ["SM4", 40, 43],
+        ["DP", 44, 47],
+        ["FHM", 48, 51],
+        ["TS", 52, 55],
+        # 56-59 reserved
+        ["RNDR", 60, 63]
+]
+
+pfr0_matrix = [
+        ["EL0", 0, 3],
+        ["EL1", 4, 7],
+        ["EL2", 8, 11],
+        ["EL3", 12, 15],
+        ["FP", 16, 19],
+        ["AdvSIMD", 20, 23],
+        ["GIC", 24, 27],
+        # 28-31 reserved
+        ["SVE", 32, 35],
+        # 36-47 reserved
+        ["DIT", 48, 51]
+        # 52-63 reserved
+]
+
+pfr1_matrix = [
+        ["BT", 0, 3],
+        ["SSBS", 4, 7],
+        ["MTE", 8, 11],
+        # 12 - 63 reserved
+]
+
+mmfr0_matrix = [
+        # 0 - 59 reserved
+        ["ECV", 60, 63]
+]
+
+# Matrices for features with >2 possibilities
+# [name, dict with possible values, start_bit, stop_bit]
+clidr_matrix = [
+        [
+                "CType1",
+                {
+                        0b011: "Separate instruction and data caches at L1."
+                },
+                0, 2
+        ],
+        [
+                "CType2",
+                {
+                        0b100: "L2 cache is implemented as a unified cache."
+                },
+                3, 5
+        ],
+        [
+                "Ctype3",
+                {
+                        0b000: "Both per-core L2 and cluster L3 caches are present.",
+                        0b100: "It is not true that both per-core L2 and cluster L3 caches are present."
+                },
+                6, 8
+        ],
+        # 9 - 20 reserved
+        [
+                "LoUIS",
+                {
+                        0b000: "No cache level needs cleaning to Point of Unification."
+                },
+                21, 23
+        ],
+        [
+                "LoC",
+                {
+                        0b010: "L3 cache is not implemented",
+                        0b011: "L3 cache is implemented"
+                },
+                24, 26
+        ],
+        [
+                "LoUU",
+                {
+                        0b000: "No levels of cache need to be cleaned or invalidated when cleaning or invalidating to the Point of Unification. This is the value if no caches are configured"
+                },
+                27, 29
+        ],
+        [
+                "ICB",
+                {
+                        0b010: "L2 cache is the highest inner level",
+                        0b011: "L3 cache is the highest inner level"
+                },
+                30, 32
+        ]
+]
+
+midr_matrix = [
+        # [
+        #         "Part number",
+        #         partnum_doubledict,
+        #         4, 15
+        # ],
+        [
+               "Architecture",
+               architecture_dict,
+               16, 19
+        ],
+        [
+                "Implementer",
+                implementer_dict,
+                24, 31
+        ]
+]
---
+++ main.py
@@ -0,0 +1,83 @@
+# SPDX-License-Identifier: GPL-2.0-only
+
+import constants
+
+def gen_bitmask(start, stop):
+        i = 0
+        ret = 0
+
+        if start > stop:
+                raise Exception(f"The start bit ({start}) is higher than stop ({stop})!")
+        
+        if start == stop:
+                return 1 << start
+        
+        while i < (stop - start):
+                ret |= 1 << (start+i)
+                i += 1
+
+        return ret
+
+def get_nth_bits(val, start, stop):
+        return (val & gen_bitmask(start, stop)) >> start
+
+def dict_value_if_known(d: dict, key):
+        return d[key] if key in d.keys() else f"Unknown ({bin(key)})"
+
+def doubledict_value_if_known(dd: dict, key1, key2):
+        if key1 in dd.keys():
+                if key2 in dd[key1].keys():
+                        return dd[key1][key2]
+        return f"Unknown ({bin(key1)}, {bin(key2)})"
+
+def supported_or_not(val, mask):
+        return "√" if val & mask > 0 else "X"
+
+def print_supported_features(val, mtx):
+        for i in range(len(mtx)):
+                print(f"{mtx[i][0]}: {supported_or_not(val, gen_bitmask(mtx[i][1], mtx[i][2]))}")
+
+def print_features(val, mtx, implementer):
+        for i in range(len(mtx)):
+                # Yay, special case for partnum..
+                if isinstance(list(mtx[i][1].values())[0], dict):
+                        print(f"{mtx[i][0]}: {implementer}")
+                ret = dict_value_if_known(mtx[i][1], get_nth_bits(val, mtx[i][2], mtx[i][3]))
+                print(f"{mtx[i][0]}: {ret}")
+
+
+# reg_name, corresponding_matrix, is_simple
+supported_regs = [
+        ["ID_AA64MMFR0_EL1", constants.mmfr0_matrix, True],
+        ["ID_AA64ISAR0_EL1", constants.isar0_matrix, True],
+        ["ID_AA64PFR0_EL1", constants.pfr0_matrix, True],
+        ["ID_AA64PFR1_EL1", constants.pfr1_matrix, True],
+        ["CLIDR_EL1", constants.clidr_matrix, False],
+        ["MIDR_EL1", constants.midr_matrix, False]
+]
+
+def main():
+        n_regs = len(supported_regs)
+        input_arr = [None for _ in range(n_regs)]
+
+        for i in range(n_regs):
+                input_arr[i] = int(input(f"Please input the value of {supported_regs[i][0]}: "))
+
+        print()
+        print("------")
+        print()
+
+        # Hacky, but implementer value is needed for partnum, eh..
+        implementer = constants.implementer_dict[get_nth_bits(input_arr[5], 24, 31)]
+
+        for i in range(n_regs):
+                print(f"* {supported_regs[i][0]}:")
+                if supported_regs[i][2]:
+                        print_supported_features(input_arr[i], supported_regs[i][1])
+                else:
+                        print_features(input_arr[i], supported_regs[i][1], implementer)
+                print()
+
+
+if __name__ == "__main__":
+        main()
